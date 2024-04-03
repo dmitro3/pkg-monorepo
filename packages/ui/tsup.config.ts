b512a49ac7429e2347d57e5075a7b17d4c646323
WINRLabs <justb.serbia@gmail.com>
2024-04-03 12:31:31 +0300

    fix: add cjs and remove clean

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   dts: true,
-  clean: true,
   sourcemap: true,
   external: ["react", "react-dom"],
   minify: true,
