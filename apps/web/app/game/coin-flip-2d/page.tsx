803a753b7eca0ac62a2d9e59e2ff566504020bac
WINRLabs <justb.serbia@gmail.com>
2024-07-12 16:00:13 +0300

    env

--- apps/web/app/game/coin-flip-2d/page.tsx
+++ apps/web/app/game/coin-flip-2d/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import { CoinFlipGame } from "@winrlabs/web3-games";
-import { useClient, useConnect } from "wagmi";
-import { reconnect } from "wagmi/actions";
-import { config } from "../wagmi";
-import { useEffect } from "react";
 
 export default function CoinFlipPage() {
   return (
