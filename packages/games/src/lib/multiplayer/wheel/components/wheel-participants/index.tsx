77109dd1a96cce32fe9bfa8ceed5bce5256313c0
WINRLabs <justb.serbia@gmail.com>
2024-07-29 12:38:25 +0300

    wheel scroll fix

--- packages/games/src/lib/multiplayer/wheel/components/wheel-participants/index.tsx
+++ packages/games/src/lib/multiplayer/wheel/components/wheel-participants/index.tsx
@@ -13,7 +13,7 @@ const WheelParticipants = () => {
   ]);
 
   return (
-    <div className="wr-relative wr-h-[640px] wr-w-full  ">
+    <div className="wr-relative wr-h-[640px] wr-w-full ">
       <Button
         variant="outline"
         type="button"
