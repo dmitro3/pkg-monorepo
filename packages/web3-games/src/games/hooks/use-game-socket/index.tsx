513093f455480809105cf9e3c19d29a698098921
WINRLabs <justb.serbia@gmail.com>
2024-08-10 00:45:52 +0300

    fix game socket provider

--- packages/web3-games/src/games/hooks/use-game-socket/index.tsx
+++ packages/web3-games/src/games/hooks/use-game-socket/index.tsx
@@ -5,12 +5,10 @@ import React from "react";
 import { io, Socket } from "socket.io-client";
 
 interface GameSocket {
-  socket: Socket | null;
   bundlerWsUrl: string;
   network: BundlerNetwork;
 }
 const GameSocketContext = React.createContext<GameSocket>({
-  socket: null,
   bundlerWsUrl: "",
   network: BundlerNetwork.WINR,
 });
@@ -24,50 +22,9 @@ export const GameSocketProvider: React.FC<{
   network: BundlerNetwork;
   children: React.ReactNode;
 }> = ({ bundlerWsUrl, network, children }) => {
-  const { address } = useCurrentAccount();
-
-  const [socket, setSocket] = React.useState<Socket | null>(null);
-
-  React.useEffect(() => {
-    if (!address || !bundlerWsUrl || !network) return;
-    console.log(network, bundlerWsUrl, "bundler ws url");
-    setSocket(
-      io(bundlerWsUrl, {
-        extraHeaders: {
-          "x-address": address,
-          "x-network": network,
-        },
-      })
-    );
-  }, [address, bundlerWsUrl, network]);
-
-  // socket connection
-  React.useEffect(() => {
-    if (!socket) return;
-
-    socket.connect();
-
-    socket.on("connect", () => {
-      console.log("socket connected!", socket);
-    });
-
-    socket.on("disconnect", () => {
-      console.log("socket disconnected");
-    });
-
-    return () => {
-      socket.off("connect");
-
-      socket.off("disconnect");
-
-      socket.disconnect();
-    };
-  }, [socket]);
-
   return (
     <GameSocketContext.Provider
       value={{
-        socket,
         bundlerWsUrl,
         network,
       }}
--- packages/web3-games/src/games/hooks/use-listen-game-event/index.tsx
+++ packages/web3-games/src/games/hooks/use-listen-game-event/index.tsx
@@ -3,6 +3,8 @@ import SuperJSON from "superjson";
 
 import { DecodedEvent, Event } from "../../utils";
 import { useGameSocketContext } from "../use-game-socket";
+import { Socket, io } from "socket.io-client";
+import { useCurrentAccount } from "@winrlabs/web3";
 
 export const useListenGameEvent = () => {
   const [gameEvent, setGameEvent] = React.useState<DecodedEvent<
@@ -10,7 +12,47 @@ export const useListenGameEvent = () => {
     any
   > | null>(null);
 
-  const { socket } = useGameSocketContext();
+  const [socket, setSocket] = React.useState<Socket | null>(null);
+
+  const { address } = useCurrentAccount();
+
+  const { bundlerWsUrl, network } = useGameSocketContext();
+
+  // socket connection
+  React.useEffect(() => {
+    if (!socket) return;
+
+    socket.connect();
+
+    socket.on("connect", () => {
+      console.log("socket connected!", socket);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("socket disconnected");
+    });
+
+    return () => {
+      socket.off("connect");
+
+      socket.off("disconnect");
+
+      socket.disconnect();
+    };
+  }, [socket]);
+
+  React.useEffect(() => {
+    if (!address || !bundlerWsUrl || !network) return;
+    console.log(network, bundlerWsUrl, "bundler ws url");
+    setSocket(
+      io(bundlerWsUrl, {
+        extraHeaders: {
+          "x-address": address,
+          "x-network": network,
+        },
+      })
+    );
+  }, [address, bundlerWsUrl, network]);
 
   React.useEffect(() => {
     if (!socket) return;
