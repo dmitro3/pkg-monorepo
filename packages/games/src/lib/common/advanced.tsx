0b6751907963152a638463d512b65b2a1646be36
WINRLabs <justb.serbia@gmail.com>
2024-08-14 19:04:52 +0300

    fix hydration

--- packages/games/src/lib/common/advanced.tsx
+++ packages/games/src/lib/common/advanced.tsx
@@ -34,21 +34,24 @@ export const Advanced: React.FC<Props> = ({ children, hideSm = true }) => {
             className={
               'wr-group wr-flex wr-flex-1 wr-items-center wr-justify-between wr-pb-4 wr-font-medium wr-transition-all'
             }
+            asChild
           >
-            <Label className="wr-text-white wr-font-semibold">Advanced</Label>
-            <Switch
-              className="wr-h-5 wr-w-10 wr-rounded-md wr-bg-zinc-800 data-[state=checked]:wr-bg-lime-600"
-              thumbClassName="wr-rounded-sm wr-bg-zinc-500 wr-h-5 wr-w-5 wr-border-zinc-500 data-[state=checked]:wr-border-white data-[state=checked]:wr-bg-white"
-              id="advanced-switch"
-              checked={accordionValue === 'advanced'}
-              onCheckedChange={(checked) => {
-                if (checked) {
-                  setAccordionValue('advanced');
-                } else {
-                  setAccordionValue('');
-                }
-              }}
-            />
+            <span>
+              <Label className="wr-text-white wr-font-semibold">Advanced</Label>
+              <Switch
+                className="wr-h-5 wr-w-10 wr-rounded-md wr-bg-zinc-800 data-[state=checked]:wr-bg-lime-600"
+                thumbClassName="wr-rounded-sm wr-bg-zinc-500 wr-h-5 wr-w-5 wr-border-zinc-500 data-[state=checked]:wr-border-white data-[state=checked]:wr-bg-white"
+                id="advanced-switch"
+                checked={accordionValue === 'advanced'}
+                onCheckedChange={(checked) => {
+                  if (checked) {
+                    setAccordionValue('advanced');
+                  } else {
+                    setAccordionValue('');
+                  }
+                }}
+              />
+            </span>
           </AccordionPrimitive.Trigger>
         </AccordionPrimitive.Header>
         <AccordionContent>{children}</AccordionContent>
